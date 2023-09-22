d941fb4de572e7b9eb6273902fb3bab52a18b036
Mahmudul Hasan <me@imahmud.com>
2023-09-22 23:52:26 +0600

    setup plugin

---
+++ extended-search.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       Extended Search
+ * Plugin URI:        https://github.com/H-Mahmud/extended-search/
+ * Description:       Replace default WordPress search with custom table search.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mahmudul Hasan
+ * Author URI:        https://github.com/H-Mahmud/
+ * License:           UNLICENSED
+ * Text Domain:       extended-search
+ * Domain Path:       /languages
+ */
+defined('ABSPATH') || exit();
